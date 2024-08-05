b98bc4e72a70db2818a0ecdf2a617f0c6570ca43
golibjon <golibtoramurodov@gmail.com>
2024-08-05 11:59:58 +0500

    hjv

--- Lessons/packaging/flutter_application/lib/screens/home_screen.dart
+++ Lessons/packaging/flutter_application/lib/screens/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:crazyfeyns_first_package/crazyfeyns_first_package.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    var testing = CrazyfeynsFirstPackageBase();
+    return  Scaffold(
+      body: Column(
+        children: [
+          Text(testing.toLowerCase('Hell0 How are you'))
+        ],
+      ),
+    );
   }
 }
